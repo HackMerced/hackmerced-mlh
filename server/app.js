944c903349a8ba534f09d1e0d377be005340b14d
Chaz <ctol1994@gmail.com>
2018-12-22 19:00:40 -0800

    updated schema and cleaned up server code

--- server/app.js
+++ server/app.js
@@ -11,61 +11,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 require("./config/db");
 
 const reqPath = path.join(__dirname, "..");
-app.use(express.static(reqPath + '/client/build'));
+app.use('/css', express.static(reqPath + '/client/build/css'));
+app.use('/images', express.static(reqPath + '/client/build/images'));
+app.use('/js', express.static(reqPath + '/client/build/js'));
+
+app.get('/', function(req, res) {
+    res.sendFile(reqPath + '/client/build/index.html');
+});
 
 app.post("/addemail", (req, res, next) => {
-  // var myData = new Email(req.body);
-  // myData.save()
-  // // Save to database successful it will return to the .then segment of the promise
-  // .then(item => {
-    // res.redirect('/');
-  // })
-  // // If fails, it will return to the .catch segment of the promise
-  // .catch(err => {
-  //   // 400 statusCode indicates operation fail
-  //   res.status(400).send("unable to save to database");
-  // });
-
-  const { body } = req;
-      let {
-        email
-      } = body;
-
-      email = email.toLowerCase();
-      email = email.trim();
-      // Steps:
-      // 1. Verify email doesn't exist
-      // 2. Save
-      Email.find({
-        email: email
-      }, (err, previousEmails) => {
-        if (err) {
-          return res.send({
-            success: false,
-            message: 'Error: Server error.'
-          });
-        } else if (previousEmails.length > 0) {
-          return res.send({
-            success: false,
-            message: 'Error: Email already exists.'
-          });
-        }
-        // Save the new email
-        const newEmail = new Email();
-        newEmail.email = email;
-        newEmail.save((err, email) => {
-          if (err) {
-           return res.send({
-             success: false,
-             message: 'Error: Server error.'
-           });
-         }
-         return res.send({
-           success: true,
-           message: 'Signed up!'
-         });
-        });
-    });
+  var myData = new Email(req.body);
+  myData.save()
+  .then(doc => {
+     console.log(doc);
+   })
+   .catch(err => {
+     console.error(err);
+   })
 });
 
 // Starts the server and tells it to listen on port 3000
